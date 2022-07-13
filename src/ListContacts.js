a801e6782392166b0e8b2bbf80ac62c9382fb301
Yvan Nanfack <romyvan1999@gmail.com>
2022-07-13 05:47:06 +0100

    fix(app): change listcontact from class component to function component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,32 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 
-class ListContacts extends Component {
-  render(){
-    const { contacts } = this.props
-    return (
-      <ol className="contact-list">
-        {
-          contacts.map((contact) => 
-            <li className="contact-list-item" key={contact.id}>
-              <div 
-                className="contact-avatar"
-                style={{
-                  backgroundImage: `url(${contact.avatarURL})`
-                }}
-                ></div>
-                <div className="contact-details">
-                  <p>{contact.name}</p>
-                  <p>@{contact.handle}</p>
-                </div>
-                <button className="contact-remove">
-                  Remove
-                </button>
-            </li>
-          )
-        }
-      </ol>
-    )
-  }
+const ListContacts = ({ contacts }) => {
+  return (
+    <ol className="contact-list">
+      {
+        contacts.map((contact) => 
+          <li className="contact-list-item" key={contact.id}>
+            <div 
+              className="contact-avatar"
+              style={{
+                backgroundImage: `url(${contact.avatarURL})`
+              }}
+              ></div>
+              <div className="contact-details">
+                <p>{contact.name}</p>
+                <p>@{contact.handle}</p>
+              </div>
+              <button className="contact-remove">
+                Remove
+              </button>
+          </li>
+        )
+      }
+    </ol>
+  )
 }
 
 export default ListContacts
